79cdcc16bb716aa4579cf7bdd811862d2ea9c77a
SlyNyQ <niikwetaq@gmail.com>
2026-01-29 11:47:55 +0000

    Full Evironment Setup
    Create and install all relevant packages for the project using uv-sync

---
+++ Scripts/prototype.py
@@ -0,0 +1,25 @@
+#Investment Chatbot
+# imports
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import gradio as gr
+
+# Load environment variables in a file called .env
+# Print the key prefixes to help with any debugging
+
+load_dotenv(override=True)
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+
+
+# Initialize
+
+openai = OpenAI()
+MODEL = 'gpt-4.1-mini'
+
